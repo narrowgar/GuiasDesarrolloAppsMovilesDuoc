26d28153ab983fdffb585ac50ae08f229426c065
fabian <fxdarkx@example.com>
2025-10-13 17:08:26 -0300

    clase 2.5.2

---
+++ 2.5.2/Sqlite_MVVM_ROOM-master/app/src/main/java/com/example/sqlite/ui/ProductFormState.kt
@@ -0,0 +1,10 @@
+package com.example.sqlite.ui
+
+
+data class ProductFormState(
+    val id: Long? = null,
+    val nombre: String = "",
+    val precio: String = "",
+    val stock: String = "",
+    val error: String? = null
+)
